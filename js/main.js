6592553e8ac8fa7598456bcebf2a84d09f2997a7
Suarez Conde <s7277028@bns.bns>
2019-07-04 23:29:15 -0500

    Change the experience format

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var cont = 0;
 function next(){
-    const content = d3.select("#content");
+    const content = d3.select("#contentDinamic");
     if (cont == 0) {
         d3.select("#mainDiv").style("height", "30%");
         content.selectAll("h1").text("");   
@@ -9,45 +9,24 @@ function next(){
             .attr("id", "aboutMe")
             .attr("class", "container")
             .append("p").text("I'm developer from Bogotá, Colombia. ").append("br");
-        d3.select("#aboutMe").append("p").text("Currently, I work as a PEGA system architect at Scotiabank.").append("br");
-        d3.select("#aboutMe").append("p").text("I'm a studying a master in Systems and computing engineering at Universidad de Los Andes.").append("br");
-        d3.select("#aboutMe").append("p").text("I have experience in the technology area developing software solutions in teams with agile methodologies, Scrum and cross-functional team.").append("br");
+        d3.select("#aboutMe").append("p").text("Currently, I work as a PEGA System Architect at Scotiabank.").append("br");
+        d3.select("#aboutMe").append("p").text("I'm a studying a master in Systems and Computing Engineering at Universidad de Los Andes.").append("br");
+        d3.select("#aboutMe").append("p").text("I have experience in the technology area developing software solutions in teams with agile methodologies like Scrum").append("br");
         d3.select("#aboutMe").append("p").text("I'm passionate about learning the new technologies that are changing our world. ");
         cont = cont + 1;
         return true     
     }
     if (cont == 1) {
-        d3.select("#mainDiv").style("height", "1%");
-        content.selectAll("h1").text("").append("p").text("Experience").style("font-size", "0.8em");
-        content.select("#aboutMe").remove()   
-        content.selectAll("h2").text("")
-            .append("div")
-            .attr("id", "experience")
-            .attr("class", "container");
-        const experience = content.select("#experience");
-        const scotiabank = experience.append("div").attr("id", "Scotiabank");
-        const amym = experience.append("div").attr("id", "amym");
-        const colsubsidio = experience.append("div").attr("id", "colsubsidio");
-            
-        scotiabank.append("img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Logo_Scotiabank_%28Kanada%29.svg/1280px-Logo_Scotiabank_%28Kanada%29.svg.png")
-                .attr("width", "190")
-                .attr("alt", "Scotiabank logo")
-                .attr("class", "img");
-                scotiabank.append("h4").text("PEGA System Architect (Developer) - Bogotá, Colombia.").style("font-size", "0.8em");
-                scotiabank.append("div").attr("class", "container").append("h4").text("• Work on developing new features, deployment and given support for the HR app that supports around 88k employees across the world. All this work is based on Agile methodologies like Scrum. ").style("font-size", "0.5em");   
-                amym.append("h3").append("b").text("AMYM").style("font-size", "1.5em");
-                amym.append("h4").text("Developer - Bogotá, Colombia.").attr("class", "h4").style("font-size", "0.8em");
-                amym.append("div").attr("class", "container").append("h4").text("• Worked on the development of an internal app for the automation of some admin and production process.").style("font-size", "0.5em");   
-                colsubsidio.append("img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Colsubsidio_logo.svg/1280px-Colsubsidio_logo.svg.png")
-                .attr("width", "170")
-                .attr("alt", "Colsubsidio logo")
-                .attr("class", "img");
-            colsubsidio.append("h4").text("Internship - Bogotá, Colombia.").attr("class", "h4").style("font-size", "0.8em");
-            colsubsidio.append("div").attr("class", "container").append("h4").text("• Internship in the security area, helping to create new security implementations and evaluating the vulnerabilities of the company.").style("font-size", "0.5em");   
+        d3.select("#mainDiv").style("height", "0%");
+        content.selectAll("h1").text("").append("p").text("Experience").style("font-size", "0.7em").append("hr");
+        content.select("#aboutMe").remove();
+        content.selectAll("h2").text("");
+        d3.select("#experience").style("display","");
             cont = cont + 1;
             return true     
     }
     if (cont == 2) {
+        d3.select("#experience").style("display","none");
         d3.select("#mainDiv").style("height", "30%");
         content.selectAll("h1").text("Education");   
         content.selectAll("h2").text("")
@@ -55,7 +34,7 @@ function next(){
             .attr("id", "education")
             .attr("class", "container");
         education = d3.select("#education");
-        education.append("h2").text("Universidad de Los Andes - Bogotá, Colombia   |   2018 - Expected 2020");
+        education.append("h2").text("Universidad de Los Andes - Bogotá, Colombia   |   201\8 - present");
         education.append("h3").text("Master, Systems and Computing Engineering ");
         education.append("h2").text("Universidad Católica de Colombia - Bogotá, Colombia   |   2012 - 2016");
         education.append("h3").text("Bachelor, Industrial Engineering");
@@ -107,10 +86,11 @@ function next(){
         .attr("id", "contactme")
         .attr("class", "container");
         contactme = d3.select("#contactme");
-        contactme.append("h2").text("+57 3058148948")
-        contactme.append("h2").text("santiagoconde03@gmail.com")
-        contactme.append("h2").append("a").text("LinkedIn").attr("href" , "https://www.linkedin.com/in/santiagosuarezconde/")
-        contactme.append("h2").append("a").text("GitHub").attr("href" , "https://github.com/santiagoconde0")
+        contactme.append("h2").text("+57 3058148948");
+        contactme.append("h2").text("santiagoconde03@gmail.com");
+        contactme.append("h2").append("a").text("LinkedIn").attr("href" , "https://www.linkedin.com/in/santiagosuarezconde/");
+        contactme.append("h2").append("a").text("GitHub").attr("href" , "https://github.com/santiagoconde0");
+        d3.select("#arrow").style("display", "none");
         cont = cont + 1;
         return true     
     }
